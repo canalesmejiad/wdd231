1fedd815546db39d5435049c4b326232c471d18f
canalesmejiad <david.kanale@hotmail.es>
2025-12-01 19:30:04 -0800

    cm

--- finalproject/styles/modules/ navigation.js
+++
@@ -1,6 +0,0 @@
-export function setCurrentYear() {
-    const yearSpan = document.querySelector('#current-year');
-    if (yearSpan) {
-        yearSpan.textContent = new Date().getFullYear();
-    }
-}
