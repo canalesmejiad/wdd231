ab58146885741ff34a24128f24f23f9ce2f75c40
canalesmejiad <david.kanale@hotmail.es>
2025-12-01 18:03:02 -0800

    cm

--- finalproject/scripts/main.js
+++ finalproject/scripts/main.js
@@ -1,9 +1,11 @@
 import { initNavigation } from './modules/navigation.js';
 import { setCurrentYear } from './modules/footer.js';
 import { initServicesPage } from './modules/servicesPage.js';
+import { initFormSummary } from './modules/formSummary.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     initNavigation();
     setCurrentYear();
     initServicesPage();
+    initFormSummary();
 });
